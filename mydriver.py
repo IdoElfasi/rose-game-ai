cdc243e40094e003a0702bdec2bc4539ef38bfd2
fkfk10 <aofek89@gmail.com>
2025-07-21 16:45:30 +0300

    small fixes in looks

--- mydriver.py
+++ mydriver.py
@@ -2,6 +2,7 @@ from rose.common import obstacles, actions  # NOQA
 
 driver_name = "SmartDrive v1.6.5"
 bad = [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER]
+badmove = [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]
 
 
 def check_corner(world, pos, xAdjaster):
@@ -10,28 +11,32 @@ def check_corner(world, pos, xAdjaster):
     ff = world.get(pos["ff"])
     fx = world.get(pos["f" + xAdjaster])
     ffx = world.get(pos["ff" + xAdjaster])
-    if f in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and fx in [obstacles.NONE, obstacles.PENGUIN] and ff in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and ffx in [obstacles.NONE, obstacles.WATER, obstacles.CRACK, obstacles.PENGUIN]: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if f in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and fx in [obstacles.NONE,
+                                                                                            obstacles.PENGUIN] and ff in [
+        obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and ffx in [obstacles.NONE, obstacles.WATER,
+                                                                                        obstacles.CRACK,
+                                                                                        obstacles.PENGUIN]: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
     if f in bad:
         if fx in (obstacles.PENGUIN, obstacles.NONE):
             return actions.RIGHT if xAdjaster == "r" else actions.LEFT
     else:
-        if fx in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]:
+        if fx in badmove:
             if f == obstacles.NONE: return actions.NONE
             if f == obstacles.WATER: return actions.BRAKE
             if f == obstacles.CRACK: return actions.JUMP
     print(f"ff: {ff}  ff{xAdjaster}: {ffx}")
     print(f"f: {f}  f{xAdjaster}: {fx}")
-    if world.get(pos["ff"]) == obstacles.PENGUIN :
+    if world.get(pos["ff"]) == obstacles.PENGUIN:
         if f == obstacles.NONE: return actions.NONE
         if f == obstacles.WATER: return actions.BRAKE
         if f == obstacles.CRACK: return actions.JUMP
-    if ffx == obstacles.PENGUIN : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
-    if f == obstacles.WATER : return actions.BRAKE
+    if ffx == obstacles.PENGUIN: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if f == obstacles.WATER: return actions.BRAKE
     if f == obstacles.CRACK: return actions.JUMP
-    if ff in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
-    if ffx in (obstacles.CRACK, obstacles.WATER) : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
-    if ff == obstacles.NONE : return actions.NONE
-    if ffx == obstacles.NONE : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if ff in (obstacles.CRACK, obstacles.WATER): return actions.NONE
+    if ffx in (obstacles.CRACK, obstacles.WATER): return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if ff == obstacles.NONE: return actions.NONE
+    if ffx == obstacles.NONE: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
 
 
 def check_center(world, pos):
@@ -43,21 +48,21 @@ def check_center(world, pos):
     ffr = world.get(pos["ffr"])
     possible = [actions.NONE, actions.LEFT, actions.RIGHT]
     if f in bad: possible.remove(actions.NONE)
-    if fl in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.LEFT)
-    if fr in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.RIGHT)
+    if fl in badmove: possible.remove(actions.LEFT)
+    if fr in badmove: possible.remove(actions.RIGHT)
     allSame = True if len(possible) == 0 or len(possible) == 3 else False
-    if (allSame or actions.NONE in possible) and ff == obstacles.PENGUIN :
-        if f == obstacles.NONE : return actions.NONE
+    if (allSame or actions.NONE in possible) and ff == obstacles.PENGUIN:
+        if f == obstacles.NONE: return actions.NONE
         if f == obstacles.WATER: return actions.BRAKE
         if f == obstacles.CRACK: return actions.JUMP
     if (allSame or actions.LEFT in possible) and ffl == obstacles.PENGUIN: return actions.LEFT
     if (allSame or actions.RIGHT in possible) and ffr == obstacles.PENGUIN: return actions.RIGHT
-    if f == obstacles.WATER : return actions.BRAKE
+    if f == obstacles.WATER: return actions.BRAKE
     if f == obstacles.CRACK: return actions.JUMP
-    if (allSame or actions.NONE in possible) and ff in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
+    if (allSame or actions.NONE in possible) and ff in (obstacles.CRACK, obstacles.WATER): return actions.NONE
     if (allSame or actions.LEFT in possible) and ffl in (obstacles.CRACK, obstacles.WATER): return actions.LEFT
     if (allSame or actions.RIGHT in possible) and ffr in (obstacles.CRACK, obstacles.WATER): return actions.RIGHT
-    if (allSame or actions.NONE in possible) and ff == obstacles.NONE : return actions.NONE
+    if (allSame or actions.NONE in possible) and ff == obstacles.NONE: return actions.NONE
     if (allSame or actions.LEFT in possible) and ffl == obstacles.NONE: return actions.LEFT
     if (allSame or actions.RIGHT in possible) and ffr == obstacles.NONE: return actions.RIGHT
     return actions.NONE
@@ -65,8 +70,6 @@ def check_center(world, pos):
 
 def directByLane(world, pos):
     if world.get(pos["f"]) == obstacles.PENGUIN: return actions.PICKUP
-    # if world.get(pos["f"]) == obstacles.WATER: return actions.BRAKE
-    # if world.get(pos["f"]) == obstacles.CRACK: return actions.JUMP
     if pos["s"][0] == 0 or pos["s"][0] == 3:
         return check_corner(world, pos, "r")
     if pos["s"][0] == 2 or pos["s"][0] == 5:
