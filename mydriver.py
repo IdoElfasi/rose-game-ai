eff47cad0ecad28c1b22ba86ae28d94ab584abf7
unknown <aofek89@gmail.com>
2025-07-21 11:26:42 +0300

    Revert "change to taple"
    This reverts commit c71f1ee1f9f6042108c90c3c0a259fa17efcfec1.

--- mydriver.py
+++ mydriver.py
@@ -1,6 +1,6 @@
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "Best drhaer"
+driver_name = "Best driver"
 bad = [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER]
 
 def soumthing_infrount(world, pose, obstacle):
@@ -14,40 +14,43 @@ def soumthing_infrount(world, pose, obstacle):
 
 
 def chek_corner(world, pos, xAdjaster):
-    if world.get(pos["f"]) == obstacles.PENGUIN or world.get(pos["f"+xAdjaster]) in bad:
+    if world.get((pos[0], pos[1] - 1)) == obstacles.PENGUIN or world.get((pos[0] + xAdjaster, pos[1] - 1)) in bad:
         return 0
-    if world.get(pos["f"+xAdjaster]) == obstacles.PENGUIN or world.get(pos["ff"+xAdjaster+xAdjaster]) == obstacles.PENGUIN or world.get(pos["f"]) in bad:
+    if world.get((pos[0] + xAdjaster, pos[1] - 1)) == obstacles.PENGUIN or world.get(
+            (pos[0] + 2 * xAdjaster, pos[1] - 2)) == obstacles.PENGUIN or world.get((pos[0], pos[1] - 1)) in bad:
         return xAdjaster
-    return "r" if xAdjaster == "l" else "l"
+    return -xAdjaster
 
 
 def check_center(world, pos):
-    if world.get(pos["f"]) == obstacles.PENGUIN:
+    if world.get(pos[0], pos[1] - 1) == obstacles.PENGUIN:
         return actions.NONE
-    if world.get(pos["fl"]) == obstacles.PENGUIN or( pos["s"][1] > 2 and world.get(pos["ffl"]) == obstacles.PENGUIN and world.get(pos["fl"]) not in bad):
+    if world.get(pos[0] - 1, pos[1] - 1) == obstacles.PENGUIN or( pos[1] > 2 and world.get(pos[0] - 1, pos[
+                                                                                                        1] - 2) == obstacles.PENGUIN and world.get(
+        pos[0] - 1, pos[1] - 1) not in bad):
         return actions.LEFT
-    if world.get(pos["fr"]) not in bad and world.get(pos["ffr"]==obstacles.PENGUIN):
+    if world.get(pos[0] + 1, pos[1] - 1) not in bad :
         return actions.RIGHT
     return actions.NONE
 
 def pingwin(world, pos):
-    if pos["s"][0] == 0 or pos["s"][0] == 3:
-        if chek_corner(world, pos, "r") == "r":
+    if pos[0] == 0 or pos[0] == 3:
+        if chek_corner(world, pos, 1) == 1:
             return actions.RIGHT
         return actions.NONE
-    if pos["s"][0] == 2 or pos["s"][0] == 5:
-        if chek_corner(world, pos, "l") == "l":
+    if pos[0] == 2 or pos[0] == 5:
+        if chek_corner(world, pos, -1) == -1:
             return actions.LEFT
         return actions.NONE
     return check_center(world, pos)
 
 
 def drive(world):
-    poss = {"s":(world.car.x, world.car.y),"f":(pos[0],pos[1]-1),"fl":(pos[0]-1,pos[1]-1),"fr":(pos[0]+1,pos[1]-1),"ffl":(pos[0]-1,pos[1]-2),"ffr":(pos[0]+1,pos[1]-2),"ffll":(pos[0]-2,pos[1]-2),"ffrr":(pos[0]+2,pos[1]-2),"ff":(pos[0],pos[1]-2)}
+    pos = [world.car.x, world.car.y]
     try:
-        obstacle = world.get(poss["f"])
-        if world.get(poss["f"]) == obstacles.NONE: return pingwin(world, poss)
-        return soumthing_infrount(world, poss, obstacle)
+        obstacle = world.get((pos[0], pos[1]))
     except IndexError:
         return actions.NONE
-
+    else:
+        if world.get(pos[0], pos[1] - 1) == obstacles.NONE: return pingwin(world, pos)
+        return soumthing_infrount(world, pose, obstacle)
