58be1a7861774b24afb70d9ed6642f0c92cbbebc
iotamg <yotamgojman9@gmail.com>
2025-07-21 15:45:53 +0300

    massive amount of fixes and development untracked

--- mydriver.py
+++ mydriver.py
@@ -1,46 +1,65 @@
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "SmartDrive v1.6.3"
+driver_name = "SmartDrive v1.6.5"
 bad = [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER]
 
 
 def check_corner(world, pos, xAdjaster):
-    if world.get(pos["f"]) in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and world.get(pos["f" + xAdjaster]) in [obstacles.NONE, obstacles.PENGUIN] and world.get(pos["ff"]) in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and world.get(pos["ff" + xAdjaster]) in [obstacles.NONE, obstacles.WATER, obstacles.CRACK, obstacles.PENGUIN]: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
-    if world.get(pos["f"]) in bad:
-        if world.get(pos["f" + xAdjaster]) in (obstacles.PENGUIN, obstacles.NONE):
+    print("Checking corner")
+    f = world.get(pos["f"])
+    ff = world.get(pos["ff"])
+    fx = world.get(pos["f" + xAdjaster])
+    ffx = world.get(pos["ff" + xAdjaster])
+    if f in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and fx in [obstacles.NONE, obstacles.PENGUIN] and ff in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.NONE] and ffx in [obstacles.NONE, obstacles.WATER, obstacles.CRACK, obstacles.PENGUIN]: return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if f in bad:
+        if fx in (obstacles.PENGUIN, obstacles.NONE):
             return actions.RIGHT if xAdjaster == "r" else actions.LEFT
     else:
-        if world.get(pos["f" + xAdjaster]) in bad:
-            return actions.NONE
-    if world.get(pos["ff"]) == obstacles.PENGUIN : return actions.NONE
-    if world.get(pos["ff" + xAdjaster]) == obstacles.PENGUIN : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
-    if world.get(pos["f"]) == obstacles.WATER : return actions.BRAKE
-    if world.get(pos["f"]) == obstacles.CRACK: return actions.JUMP
-    if world.get(pos["ff"]) in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
-    if world.get(pos["ff" + xAdjaster]) in (obstacles.CRACK, obstacles.WATER) : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
-    if world.get(pos["ff"]) == obstacles.NONE : return actions.NONE
-    if world.get(pos["ff" + xAdjaster]) == obstacles.NONE : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+        if fx in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]:
+            if f == obstacles.NONE: return actions.NONE
+            if f == obstacles.WATER: return actions.BRAKE
+            if f == obstacles.CRACK: return actions.JUMP
+    print(f"ff: {ff}  ff{xAdjaster}: {ffx}")
+    print(f"f: {f}  f{xAdjaster}: {fx}")
+    if world.get(pos["ff"]) == obstacles.PENGUIN :
+        if f == obstacles.NONE: return actions.NONE
+        if f == obstacles.WATER: return actions.BRAKE
+        if f == obstacles.CRACK: return actions.JUMP
+    if ffx == obstacles.PENGUIN : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if f == obstacles.WATER : return actions.BRAKE
+    if f == obstacles.CRACK: return actions.JUMP
+    if ff in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
+    if ffx in (obstacles.CRACK, obstacles.WATER) : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if ff == obstacles.NONE : return actions.NONE
+    if ffx == obstacles.NONE : return actions.RIGHT if xAdjaster == "r" else actions.LEFT
 
 
 def check_center(world, pos):
+    f = world.get(pos["f"])
+    fl = world.get(pos["fl"])
+    fr = world.get(pos["fr"])
+    ff = world.get(pos["ff"])
+    ffl = world.get(pos["ffl"])
+    ffr = world.get(pos["ffr"])
     possible = [actions.NONE, actions.LEFT, actions.RIGHT]
-    if world.get(pos["f"]) in bad: possible.remove(actions.NONE)
-    if world.get(pos["fl"]) in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.LEFT)
-    if world.get(pos["fr"]) in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.RIGHT)
+    if f in bad: possible.remove(actions.NONE)
+    if fl in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.LEFT)
+    if fr in [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER, obstacles.WATER, obstacles.CRACK]: possible.remove(actions.RIGHT)
     allSame = True if len(possible) == 0 or len(possible) == 3 else False
-    print(f"possible: {possible} | allSame {allSame}")
-    print("ff: "+ world.get(pos["ff"]) + " | ffl: " + world.get(pos["ffl"]) + " | ffr: " + world.get(pos["ffr"]) + "")
-    if (allSame or actions.NONE in possible) and world.get(pos["ff"]) == obstacles.PENGUIN : return actions.NONE
-    if (allSame or actions.LEFT in possible) and world.get(pos["ffl"]) == obstacles.PENGUIN: return actions.LEFT
-    if (allSame or actions.RIGHT in possible) and world.get(pos["ffr"]) == obstacles.PENGUIN: return actions.RIGHT
-    if world.get(pos["f"]) == obstacles.WATER : return actions.BRAKE
-    if world.get(pos["f"]) == obstacles.CRACK: return actions.JUMP
-    if (allSame or actions.NONE in possible) and world.get(pos["ff"]) in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
-    if (allSame or actions.LEFT in possible) and world.get(pos["ffl"]) in (obstacles.CRACK, obstacles.WATER): return actions.LEFT
-    if (allSame or actions.RIGHT in possible) and world.get(pos["ffr"]) in (obstacles.CRACK, obstacles.WATER): return actions.RIGHT
-    if (allSame or actions.NONE in possible) and world.get(pos["ff"]) == obstacles.NONE : return actions.NONE
-    if (allSame or actions.LEFT in possible) and world.get(pos["ffl"]) == obstacles.NONE: return actions.LEFT
-    if (allSame or actions.RIGHT in possible) and world.get(pos["ffr"]) == obstacles.NONE: return actions.RIGHT
+    if (allSame or actions.NONE in possible) and ff == obstacles.PENGUIN :
+        if f == obstacles.NONE : return actions.NONE
+        if f == obstacles.WATER: return actions.BRAKE
+        if f == obstacles.CRACK: return actions.JUMP
+    if (allSame or actions.LEFT in possible) and ffl == obstacles.PENGUIN: return actions.LEFT
+    if (allSame or actions.RIGHT in possible) and ffr == obstacles.PENGUIN: return actions.RIGHT
+    if f == obstacles.WATER : return actions.BRAKE
+    if f == obstacles.CRACK: return actions.JUMP
+    if (allSame or actions.NONE in possible) and ff in (obstacles.CRACK, obstacles.WATER) : return actions.NONE
+    if (allSame or actions.LEFT in possible) and ffl in (obstacles.CRACK, obstacles.WATER): return actions.LEFT
+    if (allSame or actions.RIGHT in possible) and ffr in (obstacles.CRACK, obstacles.WATER): return actions.RIGHT
+    if (allSame or actions.NONE in possible) and ff == obstacles.NONE : return actions.NONE
+    if (allSame or actions.LEFT in possible) and ffl == obstacles.NONE: return actions.LEFT
+    if (allSame or actions.RIGHT in possible) and ffr == obstacles.NONE: return actions.RIGHT
     return actions.NONE
 
 
