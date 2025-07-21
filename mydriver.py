e23ab5971f54d41cfc2a31ae249aaa696ca8ed9f
iotamg <yotamgojman9@gmail.com>
2025-07-21 12:34:35 +0300

    Start of development forward (think what will be better penguin or water and cracks)

--- mydriver.py
+++ mydriver.py
@@ -1,56 +1,49 @@
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "Best driver"
+driver_name = "SmartDrive v1.1"
 bad = [obstacles.TRASH, obstacles.BIKE, obstacles.BARRIER]
 
-def soumthing_infrount(world, pose, obstacle):
-    if obstacle == obstacles.PENGUIN:
-        return actions.PICKUP
-    if obstacle == obstacles.WATER:
-        return actions.BRAKE
-    if obstacle == obstacles.CRACK:
-        return actions.JUMP
-    return pingwin(world, pose)
 
-
-def chek_corner(world, pos, xAdjaster):
-    if world.get((pos[0], pos[1] - 1)) == obstacles.PENGUIN or world.get((pos[0] + xAdjaster, pos[1] - 1)) in bad:
-        return 0
-    if world.get((pos[0] + xAdjaster, pos[1] - 1)) == obstacles.PENGUIN or world.get(
-            (pos[0] + 2 * xAdjaster, pos[1] - 2)) == obstacles.PENGUIN or world.get((pos[0], pos[1] - 1)) in bad:
-        return xAdjaster
-    return -xAdjaster
+def check_corner(world, pos, xAdjaster):
+    if world.get(pos["f" + xAdjaster]) in (obstacles.PENGUIN, obstacles.NONE) and world.get(
+            pos["ff" + xAdjaster]) in (obstacles.PENGUIN, obstacles.CRACK, obstacles.WATER,actions.NONE):
+        return actions.RIGHT if xAdjaster == "r" else actions.LEFT
+    if world.get(pos["f"]) == obstacles.NONE:
+        return actions.NONE
+    return actions.RIGHT if xAdjaster == "r" else actions.LEFT
 
 
 def check_center(world, pos):
-    if world.get(pos[0], pos[1] - 1) == obstacles.PENGUIN:
-        return actions.NONE
-    if world.get(pos[0] - 1, pos[1] - 1) == obstacles.PENGUIN or( pos[1] > 2 and world.get(pos[0] - 1, pos[
-                                                                                                        1] - 2) == obstacles.PENGUIN and world.get(
-        pos[0] - 1, pos[1] - 1) not in bad):
-        return actions.LEFT
-    if world.get(pos[0] + 1, pos[1] - 1) not in bad :
-        return actions.RIGHT
+    if world.get(pos["fl"]) in (obstacles.PENGUIN, obstacles.NONE) and pos["s"][1] > 2 and world.get(pos["ffl"]) in (obstacles.PENGUIN, obstacles.CRACK, obstacles.WATER): return actions.LEFT
+    if world.get(pos["fr"]) in (obstacles.PENGUIN, obstacles.NONE) and pos["s"][1] > 2 and world.get(pos["ffr"]) in (obstacles.PENGUIN, obstacles.CRACK, obstacles.WATER): return actions.RIGHT
+    if world.get(pos["f"]) in bad:
+        if world.get(pos["fl"]) in (obstacles.PENGUIN, obstacles.NONE): return actions.LEFT
+        if world.get(pos["fr"]) in (obstacles.PENGUIN, obstacles.NONE): return actions.RIGHT
+    if world.get(pos["ff"]) == obstacles.PENGUIN : return actions.NONE
+    if world.get(pos["ffr"]) == obstacles.PENGUIN : return actions.RIGHT
+    if world.get(pos["ffl"]) == obstacles.PENGUIN : return actions.LEFT
+    if world.get(pos["ff"]) in (obstacles.CRACK, obstacles.WATER): return actions.NONE
+    if world.get(pos["ffr"]) in (obstacles.CRACK, obstacles.WATER): return actions.RIGHT
+    if world.get(pos["ffl"]) in (obstacles.CRACK, obstacles.WATER): return actions.LEFT
     return actions.NONE
 
-def pingwin(world, pos):
-    if pos[0] == 0 or pos[0] == 3:
-        if chek_corner(world, pos, 1) == 1:
-            return actions.RIGHT
-        return actions.NONE
-    if pos[0] == 2 or pos[0] == 5:
-        if chek_corner(world, pos, -1) == -1:
-            return actions.LEFT
-        return actions.NONE
+
+def directByLane(world, pos):
+    if world.get(pos["f"]) == obstacles.PENGUIN: return actions.PICKUP
+    if world.get(pos["f"]) == obstacles.WATER: return actions.BRAKE
+    if world.get(pos["f"]) == obstacles.CRACK: return actions.JUMP
+    if pos["s"][0] == 0 or pos["s"][0] == 3:
+        return check_corner(world, pos, "r")
+    if pos["s"][0] == 2 or pos["s"][0] == 5:
+        return check_corner(world, pos, "l")
     return check_center(world, pos)
 
 
 def drive(world):
-    pos = [world.car.x, world.car.y]
+    x, y = world.car.x, world.car.y
+    poss = {"s": (x, y), "f": (x, y - 1), "fl": (x - 1, y - 1), "fr": (x + 1, y - 1), "ffl": (x - 1, y - 2),
+            "ffr": (x + 1, y - 2), "ffll": (x - 2, y - 2), "ffrr": (x + 2, y - 2), "ff": (x, y - 2)}
     try:
-        obstacle = world.get((pos[0], pos[1]))
+        return directByLane(world, poss)
     except IndexError:
         return actions.NONE
-    else:
-        if world.get(pos[0], pos[1] - 1) == obstacles.NONE: return pingwin(world, pos)
-        return soumthing_infrount(world, pose, obstacle)
